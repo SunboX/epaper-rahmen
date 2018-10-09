93cafe5e75a234ab58188afd73e27a0f8bb8805d
André Fiedler <andre.fiedler@me.com>
2018-10-09 23:48:21 +0200

    WIP

--- software/OpenSenseDisplay/OpenSenseDisplay.ino
+++ software/OpenSenseDisplay/OpenSenseDisplay.ino
@@ -13,8 +13,6 @@
 // can use only half buffer size
 GxEPD2_BW<GxEPD2_750, GxEPD2_750::HEIGHT / 2> display(GxEPD2_750(/*CS=15*/ SS, /*DC=4*/ 4, /*RST=5*/ 5, /*BUSY=16*/ 16));
 
-struct tm tm;
-
 // SPI pin definition
 #define CS_PIN 15
 #define RST_PIN 5
@@ -26,12 +24,16 @@ const char *password = "SaltAndPepper101";
 //const char* ssid     = "vogtland.freifunk.net";
 //const char* password = "";
 
+const char *box_id = "5b28d3891fef04001bd7dfae";
+
 String esp_chipid;
 
 const char *host = "api.opensensemap.org";
 const int httpsPort = 443;
 
 U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;
+
+DynamicJsonBuffer jsonBuffer(2500);
 
 void setup()
 {
@@ -57,26 +59,27 @@ void setup()
   connectWifi(); // Start ConnectWifi
 }
 
-/*
-struct record_type
+struct Record
 {
   const char *updatedAt;
+  const char *temp_sensor_id;
   const char *temp_value;
   const char *temp_unit;
 };
 
-typedef struct record_type Record;
 Record record;
-*/
-const char *updatedAt;
-const char *temp_value;
-const char *temp_unit;
 
 void loop()
 {
   fetchSensorInfo();
 
   Serial.println(F("done"));
+
+  fetch24hTempHistory();
+
+  Serial.println(F("done"));
+
+  delete(jsonBuffer);
 
   Serial.println(F("drawIntoScreen..."));
 
@@ -97,9 +100,6 @@ void loop()
   delay(1000 * 60 * 5);
 }
 
-/*****************************************************************
-  /* Debug output                                                  *
-  /*****************************************************************/
 void debug_out(const String &text, const int level, const bool linebreak)
 {
   //  if (level <= debug) {
@@ -113,35 +113,6 @@ void debug_out(const String &text, const int level, const bool linebreak)
 
 void drawIntoScreen()
 {
-  /*
-    const size_t chartBufferSize = 2 * JSON_ARRAY_SIZE(12) + JSON_OBJECT_SIZE(2) + 120;
-    DynamicJsonBuffer chartJsonBuffer(chartBufferSize);
-
-    const char* chartJson = "{\"dataset\":[122,65,80,84],\"labels\":[\"Jan\",\"Feb\",\"Mar\",\"Apr\"]}";
-
-    JsonObject& chartData = chartJsonBuffer.parseObject(chartJson);
-
-    JsonArray& dataset = chartData["dataset"];
-    JsonArray& labels = chartData["labels"];
-
-    uint16_t xPadding = 30;
-    uint16_t yPadding = 30;
-
-    uint16_t height = 200;
-    uint16_t width = 200;
-  */
-
-  /*
-    chart.draw(
-       xPadding,
-       yPadding,
-       height,
-       width,
-       dataset,
-       labels)
-    ;
-  */
-
   yield();
 
   Serial.println(F("display init"));
@@ -191,7 +162,9 @@ void drawUpdatedTime()
 {
   u8g2Fonts.setFont(u8g2_font_4x6_tf);
 
-  strptime(updatedAt, "%Y-%m-%dT%H:%M:%S.%fZ", &tm);
+  struct tm tm;
+
+  strptime(record.updatedAt, "%Y-%m-%dT%H:%M:%S.%fZ", &tm);
 
   tm.tm_hour += 2; // add 2 hous to the time
   mktime(&tm);     // normalize it
@@ -209,8 +182,8 @@ void drawTemperatureBox()
   u8g2Fonts.setFont(u8g2_font_inb27_mf);
 
   u8g2Fonts.setCursor(50, 100);
-  u8g2Fonts.print(String(temp_value));
-  u8g2Fonts.print(String(temp_unit));
+  u8g2Fonts.print(String(record.temp_value));
+  u8g2Fonts.print(String(record.temp_unit));
 }
 
 /*
@@ -222,22 +195,55 @@ void drawTemperatureChart()
   u8g2Fonts.print(String(temp_value));
   u8g2Fonts.print(String(temp_unit));
 }
+*/
 
 void fetch24hTempHistory()
 {
   JsonObject &root = fetch24hTempHistoryAsObject();
 
-  JsonObject &root_0 = root[0];
-  const char *root_0_value = root_0["value"]; // "13.00"
+  Serial.println(root.size());
 
-  JsonArray &root_0_location = root_0["location"];
-  float root_0_location0 = root_0_location[0]; // 12.138863
-  float root_0_location1 = root_0_location[1]; // 50.497396
-  int root_0_location2 = root_0_location[2];   // 367
+  jsonBuffer.clear();
 
-  const char *root_0_createdAt = root_0["createdAt"]; // "2018-10-08T20:19:53.631Z"
+  /*
+  float minVal = 0.0;
+  float maxVal = 0.0;
+  
+  float data[24];
+  float sumPerHour[24] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+  int measurementsPerHour[24] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  
+  for (int i = 0; i < root.size(); i++) {
+    JsonObject& obj = root[i];
+    
+    const char* obj_value = obj["value"]; // "18.40"
+    
+    JsonArray& obj_location = obj["location"];
+    float obj_location0 = obj_location[0]; // 12.138863
+    float obj_location1 = obj_location[1]; // 50.497396
+    int obj_location2 = obj_location[2]; // 367
+    
+    const char* obj_createdAt = obj["createdAt"]; // "2018-10-09T11:05:46.821Z"
+    
+    float val = atof(obj_value);
+  
+    if (val <= minVal) minVal = val;
+    if (val >= maxVal) maxVal = val;
+    
+    / *
+    strptime(obj_createdAt, "%Y-%m-%dT%H:%M:%S.%fZ", &tm);
+
+    sumPerHour[tm.tm_hour] += val;
+    measurementsPerHour[tm.tm_hour]++;
+    
+    * /
+  }
+  
+  for (int i = 0; i < 24; i++) {
+    data[i] = sumPerHour[i] / measurementsPerHour[i];
+  }
+  */
 }
-*/
 
 void fetchSensorInfo()
 {
@@ -249,53 +255,46 @@ void fetchSensorInfo()
 
   Serial.println(F("prepare JSON object"));
 
-  const char *_id = root["_id"]; // "5b28d3891fef04001bd7dfae"
+  const char *_id = root["_id"];
 
   JsonArray &sensors = root["sensors"];
 
-  JsonObject &sensors0 = sensors[0];
-  const char *sensors0_title = sensors0["title"];           // "PM10"
-  const char *sensors0_unit = sensors0["unit"];             // "µg/m³"
-  const char *sensors0_sensorType = sensors0["sensorType"]; // "SDS 011"
-  const char *sensors0_icon = sensors0["icon"];             // "osem-cloud"
-  const char *sensors0__id = sensors0["_id"];               // "5b28d3891fef04001bd7dfb2"
+  for (int i = 0; i < sensors.size(); i++)
+  {
+    JsonObject &sensor = sensors[i];
 
-  const char *sensors0_lastMeasurement_value = sensors0["lastMeasurement"]["value"];         // "23.10"
-  const char *sensors0_lastMeasurement_createdAt = sensors0["lastMeasurement"]["createdAt"]; // "2018-10-08T20:14:55.228Z"
+    const char *sensor_title = sensor["title"];
+    const char *sensor_unit = sensor["unit"];
+    const char *sensor_sensorType = sensor["sensorType"];
+    const char *sensor_icon = sensor["icon"];
+    const char *sensor_id = sensor["_id"];
 
-  JsonObject &sensors1 = sensors[1];
-  const char *sensors1_title = sensors1["title"];           // "PM2.5"
-  const char *sensors1_unit = sensors1["unit"];             // "µg/m³"
-  const char *sensors1_sensorType = sensors1["sensorType"]; // "SDS 011"
-  const char *sensors1_icon = sensors1["icon"];             // "osem-cloud"
-  const char *sensors1__id = sensors1["_id"];               // "5b28d3891fef04001bd7dfb1"
+    const char *sensor_lastMeasurement_value = sensor["lastMeasurement"]["value"];
+    const char *sensor_lastMeasurement_createdAt = sensor["lastMeasurement"]["createdAt"];
 
-  const char *sensors1_lastMeasurement_value = sensors1["lastMeasurement"]["value"];         // "18.87"
-  const char *sensors1_lastMeasurement_createdAt = sensors1["lastMeasurement"]["createdAt"]; // "2018-10-08T20:14:55.228Z"
+    if (strcmp(sensor["title"], "PM10") == 0)
+    {
+    }
+    else if (strcmp(sensor["title"], "PM2.5") == 0)
+    {
+    }
+    else if (strcmp(sensor["title"], "Temperatur") == 0)
+    {
 
-  JsonObject &sensors2 = sensors[2];
-  const char *sensors2_title = sensors2["title"];           // "Temperatur"
-  const char *sensors2_unit = sensors2["unit"];             // "°C"
-  const char *sensors2_sensorType = sensors2["sensorType"]; // "DHT22"
-  const char *sensors2_icon = sensors2["icon"];             // "osem-thermometer"
-  const char *sensors2__id = sensors2["_id"];               // "5b28d3891fef04001bd7dfb0"
+      record.temp_sensor_id = sensor_id;
+      record.updatedAt = sensor_lastMeasurement_createdAt;
+      record.temp_value = sensor_lastMeasurement_value;
+      record.temp_unit = sensor_unit;
+    }
+    else if (strcmp(sensor["title"], "rel. Luftfeuchte") == 0)
+    {
+    }
+    else if (strcmp(sensor["title"], "Luftdruck") == 0)
+    {
+    }
+  }
 
-  const char *sensors2_lastMeasurement_value = sensors2["lastMeasurement"]["value"];         // "12.90"
-  const char *sensors2_lastMeasurement_createdAt = sensors2["lastMeasurement"]["createdAt"]; // "2018-10-08T20:14:55.228Z"
-
-  JsonObject &sensors3 = sensors[3];
-  const char *sensors3_title = sensors3["title"];           // "rel. Luftfeuchte"
-  const char *sensors3_unit = sensors3["unit"];             // "%"
-  const char *sensors3_sensorType = sensors3["sensorType"]; // "DHT22"
-  const char *sensors3_icon = sensors3["icon"];             // "osem-humidity"
-  const char *sensors3__id = sensors3["_id"];               // "5b28d3891fef04001bd7dfaf"
-
-  const char *sensors3_lastMeasurement_value = sensors3["lastMeasurement"]["value"];         // "99.90"
-  const char *sensors3_lastMeasurement_createdAt = sensors3["lastMeasurement"]["createdAt"]; // "2018-10-08T20:14:55.228Z"
-
-  updatedAt = sensors2_lastMeasurement_createdAt;
-  temp_value = sensors2_lastMeasurement_value;
-  temp_unit = sensors2_unit;
+  jsonBuffer.clear();
 }
 
 void connectWifi()
@@ -348,125 +347,54 @@ JsonObject &fetchSensorInfoAsObject()
   // Connect to HTTPS server
   WiFiClientSecure client;
 
-  String url = F("/boxes/5b28d3891fef04001bd7dfae/sensors");
-
-  //fetchUrl(&client, url);
-
-  Serial.print(F("connecting to "));
-  Serial.println(host);
-  if (!client.connect(host, httpsPort))
-  {
-    Serial.println(F("connection failed"));
-  }
-
-  yield();
-
-  Serial.print(F("requesting URL: "));
-  Serial.println(url);
-
-  yield();
-
-  // Make a HTTP request:
-  client.print(F("GET "));
-  client.print(url);
-  client.print(F(" HTTP/1.0"));
-  client.println();
-
-  client.print(F("Host: "));
-  client.print(host);
-  client.println();
-
-  client.println(F("User-Agent: OpenSenseDisplay"));
-  client.println(F("Connection: close"));
-  if (client.println() == 0)
-  {
-    Serial.println(F("Failed to send request"));
-  }
-
-  // Check HTTP status
-  char status[32] = {0};
-  client.readBytesUntil('\r', status, sizeof(status));
-  if (strcmp(status, "HTTP/1.1 200 OK") != 0)
-  {
-    Serial.print(F("Unexpected response: "));
-    Serial.println(status);
-  }
-
-  // Skip HTTP headers
-  char endOfHeaders[] = "\r\n\r\n";
-  if (!client.find(endOfHeaders))
-  {
-    Serial.println(F("Invalid response"));
-  }
-
-  yield();
-
-  /*
-    String payload;
-
-    if (httpCode == HTTP_CODE_OK) {
-    payload = client.getString();
-    } else {
-    Serial.println(httpCode);
-    Serial.printf("[HTTP] GET... failed, error: %s\n", client.errorToString(httpCode).c_str());
-
-    //    Serial.println("Going to sleep now");
-    //    esp_deep_sleep_start();
-    }
-
-    client.end();
-  */
-
-  Serial.print(F("free heap: "));
-  Serial.println(ESP.getFreeHeap());
-
-
-
-
-  Serial.println(F("parsing response"));
-
-  // Allocate JsonBuffer
-  // Use arduinojson.org/assistant to compute the capacity.
-  const size_t bufferSize = JSON_ARRAY_SIZE(5) + 6 * JSON_OBJECT_SIZE(2) + 5 * JSON_OBJECT_SIZE(6) + 900;
-  DynamicJsonBuffer jsonBuffer(bufferSize);
-
-  yield();
-
-  Serial.print(F("free heap: "));
-  Serial.println(ESP.getFreeHeap());
-
-  JsonObject &root = jsonBuffer.parseObject(client);
-
-  yield();
-
-  client.stop();
-
-  yield();
-
-  if (!root.success())
-  {
-    Serial.println(F("Parsing failed!"));
-  }
-
-  return root;
-}
-
-/*
-JsonObject &fetch24hTempHistoryAsObject()
-{
-  // Connect to HTTPS server
-  WiFiClientSecure client;
-
-  String url = F("boxes/5b28d3891fef04001bd7dfae/data/5b28d3891fef04001bd7dfb0?from-date=2018-10-07T20:22:22.729Z&to-date=2018-10-08T20:22:22.729Z");
+  String url = "/boxes/" + String(box_id) + "/sensors";
 
   fetchUrl(client, url);
 
   Serial.println(F("parsing response"));
 
-  // Allocate JsonBuffer
-  // Use arduinojson.org/assistant to compute the capacity.
-  const size_t bufferSize = 578 * JSON_ARRAY_SIZE(3) + JSON_ARRAY_SIZE(578) + 578 * JSON_OBJECT_SIZE(3) + 51380;
-  DynamicJsonBuffer jsonBuffer(bufferSize);
+  yield();
+
+  Serial.print(F("free heap: "));
+  Serial.println(ESP.getFreeHeap());
+
+  JsonObject &root = jsonBuffer.parseObject(client);
+
+  yield();
+
+  client.stop();
+
+  yield();
+
+  if (!root.success())
+  {
+    Serial.println(F("Parsing failed!"));
+  }
+
+  return root;
+}
+
+JsonObject &fetch24hTempHistoryAsObject()
+{
+  // Connect to HTTPS server
+  WiFiClientSecure client;
+
+  struct tm tm;
+
+  strptime(record.updatedAt, "%Y-%m-%dT%H:%M:%S.%fZ", &tm);
+
+  tm.tm_min -= 30;
+  mktime(&tm); // normalize it
+
+  char fromDateTime[25];
+
+  strftime(fromDateTime, 25, "%Y-%m-%dT%H:%M:%S.000Z", &tm);
+
+  String url = "/boxes/" + String(box_id) + "/data/" + String(record.temp_sensor_id) + "?from-date=" + String(fromDateTime) + "&to-date=" + String(record.updatedAt);
+
+  fetchUrl(client, url);
+
+  Serial.println(F("parsing response"));
 
   yield();
 
@@ -488,7 +416,6 @@ JsonObject &fetch24hTempHistoryAsObject()
 
   return root;
 }
-*/
 
 String IPAddress2String(const IPAddress &ipaddress)
 {
@@ -497,7 +424,7 @@ String IPAddress2String(const IPAddress &ipaddress)
   return String(myIpString);
 }
 
-void fetchUrl(WiFiClientSecure client, String url)
+void fetchUrl(WiFiClientSecure &client, String url)
 {
   Serial.print(F("connecting to "));
   Serial.println(host);
